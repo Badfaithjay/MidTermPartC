91441ab3353b8cf7a84afb57d3ffa541beb6ef94
Badfaithjay <maynajal@sheridancollege.ca>
2022-02-23 10:47:41 -0500

    final copy

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jalani
+ */
+public class TestUserprofile 
+{
+public static void main(String[] args)
+{
+   
+    	Scanner sc= new Scanner (System.in);
+
+   String name;
+   String stuff;
+    System.out.println("Whats your name");
+    	name= sc.nextLine();
+        
+            System.out.println("Whats your favorite genre for this list \"Comedy\", \"Drama\", \"Action\", \"Mystery\"");
+                	stuff= sc.nextLine();
+
+
+        
+        
+       
+
+}
+}
